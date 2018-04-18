b570d1d46728fdb839c56d00d689a63058d8a9bb
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-04-17 20:08:45 -0500

    Added the requires for spotify, twitter and request. Set up the switch statement

--- liri.js
+++ liri.js
@@ -1,6 +1,44 @@
 require("dotenv").config();
 
-var keys = require("./keys.js")
-
+var keys = require("./keys.js");
+var request = require("request");
+var spotify2 = require("spotify");
+var twitter = require("twitter");
 var spotify = new Spotify(keys.spotify);
-var twitter = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
+var fs = require("fs");
+
+var nodeArgv = process.argv;
+var action = process.argv[2];
+
+var title = "";
+
+
+
+
+
+switch (action) {
+    case "my-tweets":
+        tweets();
+        break;
+
+    case "spotify-this-song":
+        if (title) {
+            spotifySong(title)
+        } else {
+            spotifySong("The Sign")
+        };
+        break;
+
+    case "movie-this":
+        if (title) {
+            movieThis(title)
+        } else {
+            movieThis("Mr Nobody")
+        };
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+}
