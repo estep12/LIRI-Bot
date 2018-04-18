3b966264f389adaaa95a29d3170e063b27b900fe
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-04-17 20:31:57 -0500

    Started to set up functions for tweets, spotify, movies and do what I say.

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var request = require("request");
-var spotify2 = require("spotify");
+var spotify2 = require("node-spotify-api");
 var twitter = require("twitter");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -42,3 +42,38 @@ switch (action) {
         doIt();
         break;
 }
+
+
+function tweets() {
+    var params = { screen_name: "JohnDenver600" };
+    client.get("statuses/user_timeline", params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
+    });
+};
+
+
+function spotifySong(title) {
+    spotify.search({ type: "track" , query: title }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        } else{
+            
+        }
+
+        console.log(data);
+    });
+};
+
+
+function movieThis(){
+
+};
+
+
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        var text = data.split(", ")
+    })
+};
